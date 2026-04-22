20f44b724981436ec4b8a27c94e82465bc625a83
eliaskmrgo <elias.camargo@uptc.edu.co>
2026-04-22 16:45:59 -0500

    Cambios en order service y controller

--- cqrscontroller/src/main/java/co/edu/uptc/cqrscontroller/service/OrderService.java
+++ cqrscontroller/src/main/java/co/edu/uptc/cqrscontroller/service/OrderService.java
@@ -3,6 +3,7 @@ package co.edu.uptc.cqrscontroller.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 import co.edu.uptc.cqrscontroller.model.Order;
 import co.edu.uptc.cqrscontroller.repository.OrderRepository;
 
@@ -32,7 +33,7 @@ public class OrderService {
 
     @Transactional
     public void deleteOrder(Order order) {
-        orderRepository.delete(order);
-        orderEventProducer.sendMessage("delete-order-topic", order);
+        orderRepository.deleteById(order.getId()); 
+        orderEventProducer.sendMessage("delete-order-topic", order); 
     }
 }
